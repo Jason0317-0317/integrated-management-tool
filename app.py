b80dc176ea457a0d5c690a2fc3c6da5e33c02ceb
Jason0317-0317 <youchengzhang215@gmail.com>
2026-05-12 14:28:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if st.session_state.role is None:
     col1, col2, col3 = st.columns([1, 2, 1])
     
     with col2:
-        st.markdown("<h1 style='text-align: center;'>春不老薪資計算系統</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center;'>春不老統計系統</h1>", unsafe_allow_html=True)
         st.markdown("<p style='text-align: center; color: #999;'>請選擇您的角色登入系統</p>", unsafe_allow_html=True)
         
         st.markdown("<br>", unsafe_allow_html=True)
